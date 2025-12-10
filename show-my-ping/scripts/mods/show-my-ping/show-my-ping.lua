e7578869ea3d4b333997263571d3422d6b359d20
It Ankka <github@cote.mozmail.com>
2025-12-10 02:45:32 +0200

    Create my first mod: show-my-ping

---
+++ show-my-ping/scripts/mods/show-my-ping/show-my-ping.lua
@@ -0,0 +1,53 @@
+---@class DMFMod
+local mod         = get_mod("show-my-ping")
+local DESCRIPTION = "ping [average] (Show current ping)"
+
+local last_ping   = 0
+local measures    = {}
+
+---@param num number
+---@return integer
+local function round(num) return math.floor(num + 0.5) end
+
+---@param ping number
+local function add_measure(ping)
+  if type(ping) == "number" then
+    table.insert(measures, ping)
+    local remove_count = #measures - 20
+    if remove_count < 1 then
+      return
+    end
+    for _ = 1, remove_count do
+      table.remove(measures, 1)
+    end
+  end
+end
+
+---@return integer
+mod.get_average_ping = function()
+  if table.is_empty(measures) then return -1 end
+  return round(table.average(measures))
+end
+
+--@return integer
+mod.get_ping = function()
+  local ping = last_ping
+
+  if (type(ping) ~= "number") then return -1 end
+  return round(ping)
+end
+
+mod:command("ping", DESCRIPTION, function(avg)
+  if avg == nil then
+    mod:echo("PING %d", mod.get_ping())
+    return
+  end
+  mod:echo("AVERAGE PING %d", mod.get_average_ping())
+end)
+
+mod.on_all_mods_loaded = function()
+  mod:hook_safe(CLASS.PingReporter, "_take_measure", function(self, dt)
+    last_ping = self._measures[#self._measures]
+    add_measure(last_ping)
+  end)
+end
---
+++ show-my-ping/scripts/mods/show-my-ping/show-my-ping_localization.lua
@@ -0,0 +1,5 @@
+return {
+  mod_description = {
+    en = "A mod for showing the players ping at the top of the screen",
+  },
+}
